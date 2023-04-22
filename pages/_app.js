c1b9061aade7da2250c93f49198acc9e15f6d4d5
amanverma2908 <amanverma.yt@gmail.com>
2023-04-22 22:30:29 +0530

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,11 @@ import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics";
 
 function MyApp({ Component, pageProps }) {
-  return;
+  return (
   <>
     <Component {...pageProps} />
     <Analytics />
-  </>;
+  </>);
 }
 
 export default MyApp;
